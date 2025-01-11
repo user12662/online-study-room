f0dc8d7595abc7b0302d13eeb15d3f5d75526741
user12662 <adilkohanikov@gmail.com>
2025-01-11 15:35:39 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,12 @@
-<script src="/socket.io/socket.io.js"></script> //links the stupid soket file
-
-<script src="js/app.js"></script>
-
-
-const socket = io(); // Connect to the WebSocket server
+const socket = io('https://4cd45031-00db-4296-b64a-341727b9db2d-00-1ut2a93ioco3z.janeway.replit.dev/'); // Connect to the WebSocket server using the Replit URL
 
 // Handle drawing event
-socket.emit('draw', { x: 100, y: 150 }); // Sends drawing coordinates to the server
+document.addEventListener('mousemove', (e) => {
+  const data = { x: e.clientX, y: e.clientY };
+  socket.emit('draw', data); // Emit drawing coordinates
+});
 
 // Listen for drawing data from other users
 socket.on('draw', (data) => {
   console.log('Drawing received:', data);
-
-
-  const socket = io(); // Connect to the WebSocket server
-
-// Log a message when connected
-socket.on('connect', () => {
-  console.log('Successfully connected to the server');
-});
-
 });
